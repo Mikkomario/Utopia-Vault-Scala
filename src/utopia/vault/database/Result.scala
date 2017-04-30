555a0169cc0ab928c116b912ac5c8761841febb4
Mikkomario <mikko_hilpinen@hotmail.com>
2017-04-30 10:28:44 +0300

    Changed results class so that it now uses the new row class

--- src/utopia/vault/database/Result.scala
+++ src/utopia/vault/database/Result.scala
@@ -5,19 +5,39 @@ import utopia.flow.datastructure.immutable.Model
 import utopia.flow.datastructure.immutable.Constant
 
 /**
- * A result is generated based on the data retrieved from a executed database query results. 
+ * A result is generated based on the data retrieved from a executed database query. 
  * Usually results are used for accessing read database row data, but in case of an insert
- * statement, the generated indices are also available
+ * statement, the generated indices are also available.
  * @author Mikko Hilpinen
  * @since 25.4.2017
  */
-class Result(val data: Map[Table, Vector[Model[Constant]]], val generatedKeys: Vector[Int])
+class Result(val rows: Vector[Row], val generatedKeys: Vector[Int])
 {
     // COMPUTED PROPERTIES    ------------
     
     /**
-     * All database row data for all queried tables. The tables are in no specific order. This can
-     * be used as a convenience accessor when only one table has been queried
+     * The row data in model format. This should be used when no joins were present in the query and
+     * each row contains data from a single table only
      */
-    def rows = data.values.flatten
+    def rowModels = rows.map { _.toModel }
+    
+    /**
+     * Whether this result is empty and doesn't contain any meaningful data
+     */
+    def isEmpty = generatedKeys.isEmpty && rows.forall { _.isEmpty }
+    
+    
+    // OTHER METHODS    ------------------
+    
+    /**
+     * Retrieves row data concerning a certain table
+     * @param tableName The name of the table whose data is returned
+     */
+    def rowsForTable(tableName: String) = rows.map { _(tableName) }
+    
+    /**
+     * Retrieves row data concerning a certain table
+     * @param table The table whose data is returned
+     */
+    def rowsForTable(table: Table): Vector[Model[Constant]] = rowsForTable(table.name)
 }
