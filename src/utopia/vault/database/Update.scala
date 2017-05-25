8010f0c9fe5be17b04e9fd19bc22c74da979fa48
Mikkomario <mikko_hilpinen@hotmail.com>
2017-05-25 18:24:32 +0300

    Added a factory for update segments. Not tested yet.

---
+++ src/utopia/vault/database/Update.scala
@@ -0,0 +1,33 @@
+package utopia.vault.database
+
+import utopia.vault.generic.Table
+import utopia.flow.datastructure.template.Model
+import utopia.flow.datastructure.template.Property
+import scala.collection.immutable.HashSet
+
+/**
+ * This object is used for generating update statements that modify database row contents
+ * @author Mikko Hilpinen
+ * @since 25.5.2017
+ */
+object Update
+{
+    // OPERATORS    ----------------------
+    
+    def apply(table: Table, set: Model[Property]) = 
+    {
+        val valueSet = set.attributeMap.flatMap { case (name, property) => table(name).map { (_, property.value) } }
+        
+        if (valueSet.isEmpty)
+        {
+            SqlSegment.empty
+        }
+        else 
+        {
+            SqlSegment(s"UPDATE ${ table.name } SET ${ valueSet.view.map { case (column, value) => 
+                    s"${ column.columnNameWithTable } = ?" }.reduceLeft { _ + ", " + _ } }", 
+                    valueSet.map { case (_, value) => value }.toVector, Some(table.databaseName), 
+                    HashSet(table))
+        }
+    }
+}
